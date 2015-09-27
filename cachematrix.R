84a3e35b6879cfbb1eea4a694d97198206ac09ae
ShahidShk <kausar.shahid@gmail.com>
2015-09-27 12:02:56 -0400

    Update cachematrix.R
    This code creates a matrix, computes the inverse of the matrix and  caches the inversed values of the matrix.
    The base code used was provided by the Prof. Peng that computed and cached the mean of a vector.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#This function returns a list of the following functions:
+  # 1. set the matrix
+  # 2. retrieve the matrix
+  # 3. Retrieve the inverse of the matrix
+  # 4. Retreive the cached inverse of a matrix
+makeMatrix <- function(x = matrix()) {
+  #1
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  #2
+  get <- function() x
+  
+  #3
+  setinverse <- function(inverse) m <<- inverse
+  
+  #4
+  getinverse <- function() m
+  
+  # return a list of the functions
+  list( set = set,get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+# This function does the following
+  #1. Check if cached values of the inversed matrix are available using makeMatrix
+  #2. Inverse the matrix if not cached and cache the values using makeMatrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheinverse <- function(x, ...) {
+  #1
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  #2
+  x$set(mat)
+  data <- x$get()
+  
+  #inverse the matrix 
+  m <- solve(data, ...)
+  x$setinverse(m)
 }
+
+#create a 10x10 matrix
+set.seed(12345)
+uniRndNumbers = rnorm(100)
+mat = matrix(uniRndNumbers, nrow=10, ncol=10)
+
+#initialize functions to cache
+lstFuns = makeMatrix()
+
+# inverse matrix or retreive if cached
+cacheinverse(lstFuns)
+
+#print matrix
+lstFuns$get()
+
+
+
+
+
